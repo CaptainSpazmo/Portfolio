d5511a071d5d4c34fb83eab56d7d25fb8c89b9df
CaptainSpazmo <mikeyheinrici@gmail.com>
2024-02-19 17:59:18 +0100

    Revert "first Commit"
    This reverts commit 1074c2110207cf969617a00a0866b2b46b7a3c01.

--- donkey-travel/website/validators.py
+++
@@ -1,5 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_phonenumber(value):
-    if not value.isdigit():
-        raise ValidationError('Phone number must contain only numeric characters.')
